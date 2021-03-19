5fb083a3f8045ad97ac5ff5e109e630fc57814b9
Malex1982 <malex1982@mail.ru>
2021-03-19 09:55:01 +0200

    'readyupload'

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class SaveModel(models.Model):
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to='images')
     name = models.CharField(max_length=20)
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -11,8 +11,9 @@ class MyView(View):
 
     def post(self, request):
         post_form = MyForm(request.POST)
-        SaveModel.objects.create(image=post_form.data.get('image_input'), name=post_form.data.get('image_name'))
+        SaveModel.objects.create(image=request.FILES.get('image_input'), name=post_form.data.get('image_name'))
         print(post_form.data.get('image_input'))
+        return render(request, template_name='index.html')
 
 
         return render(request, template_name='index.html')
